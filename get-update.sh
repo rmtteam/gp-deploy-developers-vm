2d43c412e70bef0c96f56b94c9a7933f21de3e08
rmtteam <48514632+rmtteam@users.noreply.github.com>
2023-02-01 16:15:13 +0530

    add files

--- get-update.sh
+++ get-update.sh
@@ -1,2 +1,2 @@
 git clone https://github.com/rmtteam/gp-deploy-developers-vm.git
-sudo cp gp-deploy-developers-vm/gp-deploy-developers-vm.yml /
+sudo cp gp-deploy-developers-vm/playbook-deploy-developers-vm.yml /etc/ansible/playbook-deploy-developers-vm.yml
---
+++ start-playbook.sh
@@ -0,0 +1,1 @@
+ansible-playbook /etc/ansible/playbook-deploy-developers-vm.yml -i hosts
