2e6d07d3e1d27c53a88088266c512d1ae01446f6
rmtteam <48514632+rmtteam@users.noreply.github.com>
2023-07-17 23:49:04 +0300

    Update get-update.sh

--- get-update.sh
+++ get-update.sh
@@ -1,4 +1,4 @@
 git pull
 sudo cp playbook-deploy-developers-vm.yml /etc/ansible/playbook-deploy-developers-vm.yml
-rm -rf /etc/ansible/roles/nvm
-sudo cp -r roles/nvm /etc/ansible/roles/
+rm -rf /etc/ansible/roles/ansible-role-nvm
+sudo cp -r roles/ansible-role-nvm /etc/ansible/roles/
